d6a97dd465846741a998230f437f84baa1c77f7c
hzy <hzyitc@outlook.com>
2023-08-14 01:32:35 +0800

    buildScript: Lint

--- buildAllPlatforms.sh
+++ buildAllPlatforms.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 OUTPUT="bin/"
 PROGRAM="GenAndVerifyData"
 LDFLAGS="-s -w"
 
-mkdir -p "$OUTPUT"
-rm -f "$OUTPUT/${PROGRAM}-"*
+mkdir -p "${OUTPUT}"
+rm -f "${OUTPUT}/${PROGRAM}-"*
 
 platforms=(
 	linux/386
@@ -22,15 +23,14 @@ platforms=(
 # platforms=($(go tool dist list))
 
 for i in "${platforms[@]}"; do
-	os="$(echo "$i" | awk -F/ '{print $1}')"
-	arch="$(echo "$i" | awk -F/ '{print $2}')"
-	mips="$(echo "$i" | awk -F/ '{print $3}')"
-	if [ "$os" == "windows" ]; then
-		suffix="${os}-${arch}.exe"
-	else
-		suffix="${os}-${arch}"
-	fi
-	filename="$OUTPUT/${PROGRAM}-$suffix"
-	echo "build $filename for $i"
-	CGO_ENABLED=0 GOOS=$os GOARCH=$arch GOMIPS=$mips go build -trimpath -ldflags "$LDFLAGS" -o "$filename"
+	os="$(echo "${i}" | awk -F/ '{print $1}')"
+	arch="$(echo "${i}" | awk -F/ '{print $2}')"
+	mips="$(echo "${i}" | awk -F/ '{print $3}')"
+
+	[ "${os}" == "windows" ] && ext="exe"
+
+	filename="${OUTPUT}/${PROGRAM}-${os}-${arch}${ext:+.$ext}"
+	echo "build ${filename} for ${i}"
+	CGO_ENABLED=0 GOOS="${os}" GOARCH="${arch}" GOMIPS="${mips}" \
+		go build -trimpath -ldflags "${LDFLAGS}" -o "${filename}"
 done
